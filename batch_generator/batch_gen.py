08b5b7c1bfbe9b4e9c93224529f491e45a1a3443
Mick van Hulst <mick.vanhulst@gmail.com>
2018-04-25 13:59:26 +0200

    batchgen probs

--- batch_generator/batch_gen.py
+++ batch_generator/batch_gen.py
@@ -11,28 +11,18 @@ from IPython.core.display import HTML
 
 from scipy.sparse import csr_matrix
 
-# %matplotlib inline
-
-import os
-# print(os.listdir("./input"))
-
-
-
-
-
 def open_file(train=False, test=False, val=False):
 
     if(train == True):
         with open("../data/train.json") as datafile1: #first check if it's a valid json file or not
             train_data = json.load(datafile1)
-    # if(test == True):
-    #     with open("../input/test.json") as datafile2:  # first check if it's a valid json file or not
-    #         test_data = json.load(datafile2)
-    # if(val == True):
-    #     with open("../input/validation.json") as datafile3:  # first check if it's a valid json file or not
-    #         valid_data = json.load(datafile3)
 
     return train_data
+
+
+def prob_scores(df):
+    # Count
+    length = len(df)
 
 
 
@@ -40,18 +30,22 @@ if __name__ == "__main__":
     train_data = open_file(train=True)
 
     train_imgs_df = pd.DataFrame.from_records(train_data["images"])
-    train_imgs_df["url"] = train_imgs_df["url"]
     train_labels_df = pd.DataFrame.from_records(train_data["annotations"])
     # train_labels_df = train_labels_df["labelId"].apply(lambda x: [int(i) for i in x])
     train_df = pd.merge(train_imgs_df, train_labels_df, on="imageId", how="outer")
     train_df["imageId"] = train_df["imageId"].astype(np.int)
-    print(train_df.head())
-    print(train_df.dtypes)
+    train_df['label_str'] = train_df['labelId'].apply(lambda x: ' '.join(x))
 
-    print("####" * 10)
-    print("## Training Data.")
-    print(train_df.isna().any())
+    agg_classes = train_df.groupby(['label_str']).size().reset_index().rename(columns={0:'cnt'})
+    agg_classes['probs'] = 1 - (agg_classes['cnt'] / len(train_df))
 
+    print(agg_classes)
+
+    '''
+
+    train_df['probs'] = pd.merge(train_df, agg_classes[['label_str', 'probs']], how='left', on='label_str')
+
+    # Keras sequence to cat using apply
     train_image_arr = train_df[["imageId", "labelId"]].apply(lambda x: [(x["imageId"], int(i)) for i in x["labelId"]],
                                                              axis=1).tolist()
     train_image_arr = [item for sublist in train_image_arr for item in sublist]
@@ -65,22 +59,9 @@ if __name__ == "__main__":
 
 
     labels = train_image_mat.sum(0).astype(np.int)
-    # print(labels)
-
-    plt.figure(figsize=(30, 18))
-    labels_inds = np.arange(len(labels.tolist()[0]))
-    sns.barplot(labels_inds, labels.tolist()[0])
-    plt.xlabel('label id', fontsize=6)
-    plt.ylabel('Count', fontsize=16)
-    plt.title("Distribution of labels", fontsize=18)
-    plt.show()
-
 
 
     ## Rows = images  //  columns classes
     df_train_image_mat = pd.DataFrame(train_image_mat.toarray())
-
-    print(df_train_image_mat.iloc[2:5])
-    print("train image mat als df", list(df_train_image_mat))
-
+    '''
 
