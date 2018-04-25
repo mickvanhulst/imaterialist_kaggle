c586ddaea8604c1b617888dcbf606eb9c30685e7
ScaleRunner <dennis-verheijden@live.nl>
2018-04-25 21:36:28 +0200

    Made loading and training models modular

--- networks/inceptionv3.py
+++ networks/inceptionv3.py
@@ -1,70 +1,59 @@
 from keras.applications.inception_v3 import InceptionV3
-from keras.preprocessing import image
 from keras.models import Model
 from keras.layers import Dense, GlobalAveragePooling2D
-from keras import backend as K
-
-from keras.optimizers import SGD
+from networks.training import train_top, fine_tune
 
 from scraper.script_download_image import download_dataset
+from keras.layers import Input
 
 
-def inception_plus(n_outputs):
+def inception_v3_model(n_outputs, n_features=1024, optimizer='rmsprop', input_shape=(299, 299, 3)):
+    """
+    This method returns the inception_v3 model with custom TOP layers
+    All layers except the TOP are frozen (i.e. cannot be trained)
+    :param n_outputs: number of output classes
+    :param n_features: number of hidden units in the feature layer
+    :param optimizer: optimizer to be used
+    :param input_shape: input shape to the model
+    :return: custom inception_v3 model
+    """
+    input_tensor = Input(shape=input_shape)
+
     # create the base pre-trained model
-    base_model = InceptionV3(weights='imagenet', include_top=False)
+    base_model = InceptionV3(input_tensor=input_tensor, weights='imagenet', include_top=False)
 
     # add a global spatial average pooling layer
     x = base_model.output
     x = GlobalAveragePooling2D()(x)
     # Feature Layer
-    x = Dense(1024, activation='relu')(x)
+    x = Dense(n_features, activation='relu')(x)
     # Prediction layer, Probability per class
     predictions = Dense(n_outputs, activation='softmax')(x)
 
     # this is the model we will train
     model = Model(inputs=base_model.input, outputs=predictions)
 
+    # freeze all InceptionV3 layers
+    for layer in model.layers[:-2]:
+        layer.trainable = False
+
     # compile the model (should be done *after* setting layers to non-trainable)
+    model.compile(optimizer=optimizer, loss='categorical_crossentropy')
+
     model.summary()
     return model, base_model
 
 
-def train_model(model, base_model):
-    # first: train only the top layers (which were randomly initialized)
-    # i.e. freeze all convolutional InceptionV3 layers
-    for layer in model.layers[:-2]:
-        layer.trainable = False
-    model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+def test():
+    download_dataset('../data/train.json', '../data/img', 1000)
 
-    # TODO: train the model on the new data for a few epochs
-    # model.fit_generator()
+    model, base_model = inception_v3_model(100)
 
-    # second: fine-tuning convolutional layers from inception V3. Freeze
-    # the bottom N layers and train the remaining top layers.
-
-    # Visualize layer names and indices to see how many layers we should freeze:
-    for i, layer in enumerate(base_model.layers):
-        print(i, layer.name)
-
-    # we chose to train the top 2 inception blocks, i.e. we will freeze
-    # the first 249 layers and unfreeze the rest:
-    for layer in model.layers[:249]:
-        layer.trainable = False
-    for layer in model.layers[249:]:
-        layer.trainable = True
-
-    # we need to recompile the model for these modifications to take effect
-    # we use SGD with a low learning rate
-    model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy')
-
-    # TODO: train the model again (this time fine-tuning the top 2 inception blocks alongside the top Dense layers
-    # model.fit_generator()
-
-    return model
+    # TODO: replace None with generator
+    train_top(None, model, base_model)
+    # we chose to train the top 2 inception blocks, i.e. we will freeze the first 249 layers
+    fine_tune(None, model, idx_lower=249)
 
 
 if __name__ == "__main__":
-    download_dataset('../data/train.json', '../data/img', 1000)
-
-    model, base_model = inception_plus(100)
-    train_model(model, base_model)
+    test()
