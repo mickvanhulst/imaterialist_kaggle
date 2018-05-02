1c12be913be7f85b771b790a926313b71f472cd4
Brian Westerweel <b.wester.1993@gmail.com>
2018-05-02 12:32:29 +0200

    removed unneccesary comments in evaluation and made F1-score micro

--- evaluation/f1_score.py
+++ evaluation/f1_score.py
@@ -1,10 +1,6 @@
 from keras.callbacks import Callback
 from sklearn.metrics import f1_score
-import numpy as np
 
-# self.model.validation_data[0] = train_x
-# self.model.validation_data[1] = train_y
-# self.model.validation_data[2] = sample_weight
 
 class micro_averaged_f1(Callback):
     def __init__(self, test_data):
@@ -18,16 +14,13 @@ class micro_averaged_f1(Callback):
         self.tn = 0
 
     def on_batch_end(self, batch, logs={}):
-        # print(logs)
         self.test += 5
 
     def on_epoch_end(self, epoch, logs={}):
         x, y = self.test_data
         y_pred = self.model.predict(x)
-        # print(y_pred)
-        # print(self.test_data)
-        # print(self.test)
         y_pred[y_pred > 0.5] = 1
         y_pred[y_pred <= 0.5] = 0
 
-        print(f1_score(self.test_data[1], y_pred))
+        print(f1_score(self.test_data[1], y_pred, average='micro'))
+        
