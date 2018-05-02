f60fed90d2d62cb24075f6108ab5f02d8b896f1c
ScaleRunner <dennis-verheijden@live.nl>
2018-05-02 14:06:46 +0200

    Added model loading if the model exists

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from batch_generator.batch_gen import DataGenerator
 from networks.inceptionv3 import inception_v3_model
 from networks import training
 from evaluation.callbacks import get_callbacks
+import os.path
+from keras.models import load_model
 
 
 def main():
     print("Setting up Model...")
+    model_name = "inception_v3"
     model, base_model = inception_v3_model(n_classes, input_shape=input_dim)
 
     print("Creating Data Generators...")
@@ -15,8 +18,10 @@ def main():
     print("Training batches:", len(training_generator))
     print("Validation batches:", len(validation_generator))
 
-    training.set_callbacks(get_callbacks("inception_v3", None))
-
+    training.set_callbacks(get_callbacks("model_name", None))
+    if os.path.isfile("./best_model_{}.h5".format(model_name)):
+        model = load_model("./best_model_{}.h5".format(model_name))
+    
     history = training.train_top(generator_train=training_generator, generator_val=validation_generator,
                                  model=model, base_model=base_model,
                                  steps_per_epoch=50, val_percentage=0.05)
