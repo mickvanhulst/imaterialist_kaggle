d6b8ae37c95d0423a4a25d16f878e0a9d6c67ba2
Verheijden, D.M.J. (Dennis) <d.verheijden@student.ru.nl>
2018-05-08 23:05:48 +0200

    Implemented the new Generator

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from batch_generator.batch_gen import DataGenerator
+from batch_generator.batch_gen import MultiLabelGenerator
 from networks.inceptionv3 import inception_v3_model
 from networks import training
 from evaluation.callbacks import get_callbacks
@@ -12,13 +12,16 @@ def main():
     model, base_model = inception_v3_model(n_classes, input_shape=input_dim)
 
     print("Creating Data Generators...")
-    training_generator = DataGenerator(datafile='./data/train.json')
-    validation_generator = DataGenerator(datafile='./data/validation.json')
+    training_generator = MultiLabelGenerator(preprocessing_function=inception_v3_model, horizontal_flip=True)
+    training_generator = training_generator.make_datagenerator(datafile='./data/train.json')
+
+    validation_generator = MultiLabelGenerator(preprocessing_function=inception_v3_model, horizontal_flip=True)
+    validation_generator = validation_generator.make_datagenerator(datafile='./data/validation.json')
 
     print("Training batches:", len(training_generator))
     print("Validation batches:", len(validation_generator))
 
-    training.set_callbacks(get_callbacks("model_name", None))
+    training.set_callbacks(get_callbacks("model_name", validation_generator))
     
     if os.path.isfile("./best_model_{}.h5".format(model_name)):
         print("Loading existing model ...")
@@ -26,7 +29,7 @@ def main():
 
     history = training.train_top(generator_train=training_generator, generator_val=validation_generator,
                                  model=model, base_model=base_model,
-                                 steps_per_epoch=50, val_percentage=0.05)
+                                 steps_per_epoch=50, val_percentage=0.05, epochs=10)
 
 
 if __name__ == "__main__":
