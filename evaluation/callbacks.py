d278cfed0f6be3a79decbd1cccaf01b704426569
Verheijden, D.M.J. (Dennis) <d.verheijden@student.ru.nl>
2018-05-23 19:44:02 +0200

    Added verbosity flags for the callbacks

--- evaluation/callbacks.py
+++ evaluation/callbacks.py
@@ -1,56 +1,28 @@
 from evaluation.f1_score import AveragedF1
-from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
-from tensorflow.python.lib.io import file_io
-import os
-from keras.callbacks import Callback
-
-class SaveModel(Callback):
-    def __init__(self, job_dir, GCP):
-        """
-        The F1 score averaged over all classes
-        :param validation_generator:
-        :param threshold:
-        :param steps: number of validation batches, default to whole validation dataset
-        """
-        super().__init__()
-        self.best_F1 = 0
-        self.job_dir = job_dir
-        self.GCP = GCP
-
-    def on_epoch_end(self, epoch, logs={}):
-        if(logs['F1'] > self.best_F1):
-            self.best_F1 = logs['F1']
-            if self.GCP:
-                # Save model
-                # Save the model locally
-                self.model.save('model.h5')
-                file_path = 'model.h5'
-                with file_io.FileIO(file_path, mode='rb') as input_f:
-                    with file_io.FileIO(os.path.join(self.job_dir, file_path), mode='wb+') as output_f:
-                        output_f.write(input_f.read())
-                        print("Saved model.h5 to GCS")
-            else:
-                self.model.save('best_model.h5')
+from evaluation.SaveModel import SaveModel
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 
 
-def get_callbacks(job_dir, test_generator, GCP=False, val_steps=None):
+def get_callbacks(job_dir, val_generator, GCP=False, val_steps=None, verbose=1):
 
     callbacks = []
 
-    if test_generator is not None:
+    if val_generator is not None:
         callbacks.append(
-            AveragedF1(test_generator, steps=val_steps))
+            AveragedF1(val_generator, steps=val_steps)
+        )
         metric = "F1"
     else:
         metric = "categorical_accuracy"
 
     callbacks.append(
-        SaveModel(job_dir, GCP))
+        SaveModel(job_dir, GCP)
+    )
 
     callbacks.append(
         ReduceLROnPlateau(monitor=metric,
                           patience=1,
-                          verbose=1,
+                          verbose=verbose,
                           mode='max')
     )
 
@@ -59,7 +31,7 @@ def get_callbacks(job_dir, test_generator, GCP=False, val_steps=None):
                       min_delta=0.0001,
                       patience=5,
                       mode='max',
-                      verbose=1)
+                      verbose=verbose)
     )
 
     return callbacks
