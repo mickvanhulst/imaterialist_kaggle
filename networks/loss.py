87ae2c2525cec7345f2b69ed444fe854bba45e22
Verheijden, D.M.J. (Dennis) <d.verheijden@student.ru.nl>
2018-05-23 03:09:56 +0200

    Fixed loading models with custom loss functions

--- networks/loss.py
+++ networks/loss.py
@@ -3,20 +3,32 @@ from functools import partial, update_wrapper
 from keras import backend as K
 import tensorflow as tf
 
+import numpy as np
 
-def weighted_categorical_crossentropy(weights):
-    weights = K.variable(weights)
-
-    def loss(y_true, y_pred):
-        _loss = y_true * K.log(y_pred)
-        _loss = tf.multiply(_loss, weights)
-        _loss = -K.sum(_loss, -1)
-        return _loss
-
-    return loss
+weights = np.ones((228,))
 
 
-def actual_correct(y_true, y_pred):
-    loss = tf.multiply(y_true, y_pred)
-    loss = 100 / K.sum(loss, -1)
-    return loss
+def weighted_categorical_crossentropy(class_weights):
+    global weights
+    weights = K.variable(class_weights)
+
+    return _weighted_categorical_crossentropy
+
+
+def _weighted_categorical_crossentropy(y_true, y_pred):
+    _loss = y_true * K.log(y_pred)
+    _loss = tf.multiply(_loss, weights)
+    _loss = -K.sum(_loss, -1)
+    return _loss
+
+
+def weighted_mean_squared_error(class_weights):
+    global weights
+    weights = K.variable(class_weights)
+
+    return _weighted_mean_squared_error
+
+
+def _weighted_mean_squared_error(y_true, y_pred):
+    _loss = K.mean(K.square(y_pred - y_true) * weights, axis=-1)
+    return _loss
--- utils/load_model.py
+++ utils/load_model.py
@@ -1,16 +1,16 @@
 import keras
+import networks.loss as loss
 from keras.utils.generic_utils import CustomObjectScope
 import os
 
 
 def load_model(path):
     if os.path.isfile(path):
-        if "mobilenet" in path:
-            with CustomObjectScope(
-                    {'relu6': keras.applications.mobilenet.relu6,
-                     'DepthwiseConv2D': keras.applications.mobilenet.DepthwiseConv2D}):
-                return keras.models.load_model(path)
-        else:
+        with CustomObjectScope(
+                {'relu6': keras.applications.mobilenet.relu6,
+                 'DepthwiseConv2D': keras.applications.mobilenet.DepthwiseConv2D,
+                 '_weighted_categorical_crossentropy': loss._weighted_categorical_crossentropy,
+                 '_weighted_mean_squared_error': loss._weighted_mean_squared_error}):
             return keras.models.load_model(path)
     else:
         raise Exception("Model {} not found!".format(path))
