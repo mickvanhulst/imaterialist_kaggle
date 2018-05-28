b2d66b591c1b24141d2d96369d302d520b4e4510
Verheijden, D.M.J. (Dennis) <d.verheijden@student.ru.nl>
2018-05-28 12:44:47 +0200

    Creating submissions now correctly handles a list of thresholds

--- evaluation/submision.py
+++ evaluation/submision.py
@@ -1,15 +1,18 @@
 import numpy as np
 
 
-def create_submission(test_generator, model, steps=None, threshold=0.5):
+def create_submission(test_generator, model, thresholds=None, steps=None):
     """
-    Create a submission for Kaggle
+    Create a submission for Kaggle using a single model
     :param test_generator:
     :param model:
     :param steps: number of batches to be evaluated, setting this to an other value creates invalid submissions,
                   just here for debugging probably.
-    :param threshold: cutoff value for which a label is 1
+    :param thresholds: cutoff value for which a label is 1
     """
+    if thresholds is None:
+        thresholds = [0.5 for _ in range(228)]
+
     preds = model.predict_generator(test_generator,
                                     steps=steps,
                                     verbose=1)
@@ -19,7 +22,7 @@ def create_submission(test_generator, model, steps=None, threshold=0.5):
     with open("submission.csv", "w") as f:
         f.write("image_id,label_id\n")
         for i, pred in enumerate(preds):
-            positive_samples = np.argwhere(pred > threshold).flatten()
+            positive_samples = np.argwhere(pred > thresholds).flatten()
             # In keras, they are 0-based, but we need them 1 based
             positive_samples += 1
 
