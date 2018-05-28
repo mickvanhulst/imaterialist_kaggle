2b996c6c62dd53b82bdce69acca2b7e59ce88279
Verheijden, D.M.J. (Dennis) <d.verheijden@student.ru.nl>
2018-05-28 11:52:15 +0200

    Added Xception boiii

--- networks/README.md
+++ networks/README.md
@@ -1,5 +1,6 @@
 # Networks
-Currently, we implemented 3/4-ish networks:
+Currently, we implemented 4/5-ish networks:
+* Xception (Total Params: __21.3 mil__)
 * Inception V3 (Total Params: __22.3 mil__)
 * Resnet 50 (Total Params: __26 mil__)
 * MobileNet (Total Params: __3.46 mil__)
@@ -8,6 +9,7 @@ Currently, we implemented 3/4-ish networks:
 ## Freezing Layers
 We have routines for training only the added top layers and freezing the base layers. These work as is. However, there is also a routine which is able to freeze the first N layers and train the rest. In this section, we will report noteworthy layers to train from the base models.
 
+* __Xception__: Training the top 2 Xception Blocks; _freeze the first __115__ layers_
 * __Inception V3__: Training the top 2 Inception Blocks; _freeze the first __249__ layers_
 * __ResNet 50__: Training from _res5c_branch2a_; _freeze the first __163__ layers_
 * __MobileNet__: Training the top 2 Conv Blocks; _freeze the first __69__ layers_
---
+++ networks/xception.py
@@ -0,0 +1,48 @@
+from keras.applications.xception import Xception
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras.models import Model
+
+from utils import params
+
+
+def xception_model(n_outputs, n_features=1024, optimizer='rmsprop', input_shape=(299, 299, 3)):
+    """
+    This method returns the Xception model with custom TOP layers
+    All layers except the TOP are frozen (i.e. cannot be trained)
+    :param n_outputs: number of output classes
+    :param n_features: number of hidden units in the feature layer
+    :param optimizer: optimizer to be used
+    :param input_shape: input shape to the model
+    :return: custom Xception model
+    """
+    # create the base pre-trained model
+    base_model = Xception(input_shape=input_shape, weights='imagenet', include_top=False)
+
+    # add a global spatial average pooling layer
+    x = base_model.output
+    x = GlobalAveragePooling2D(name="features")(x)
+
+    # Prediction layer, Probability per class
+    predictions = Dense(n_outputs, activation=params.pred_activation, name="predictions")(x)
+
+    # this is the model we will train
+    model = Model(inputs=base_model.input, outputs=predictions)
+
+    # freeze all Xception layers
+    for layer in model.layers[:-2]:
+        layer.trainable = False
+
+    # compile the model (should be done *after* setting layers to non-trainable)
+    model.compile(optimizer=optimizer, loss=params.loss)
+
+    model.summary()
+    return model, base_model
+
+
+if __name__ == "__main__":
+    model, base_model = xception_model(params.n_classes, input_shape=params.input_shape)
+
+    for i, layer in enumerate(model.layers):
+        if layer.name == "block13_sepconv1_act":
+            print("Layer {} is the beginning of the last 2 blocks".format(i))
+            break
