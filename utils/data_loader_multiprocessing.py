067591a4341c7b7df67527138f9e50381d49c5d5
Verheijden, D.M.J. (Dennis) <d.verheijden@student.ru.nl>
2018-05-30 14:01:58 +0200

    Does not save empty images

--- utils/data_loader_multiprocessing.py
+++ utils/data_loader_multiprocessing.py
@@ -8,10 +8,38 @@ import urllib3
 from PIL import Image
 from tqdm import tqdm
 
+import numpy as np
+from urllib3 import Retry
+
+import matplotlib.pyplot as plt
+
 urllib3.disable_warnings()
 tqdm.pandas()
 
-http_pool = urllib3.PoolManager(500)
+http_pool = urllib3.PoolManager(500, retries=Retry(connect=3, read=2, redirect=3))
+
+
+def pad_image(img, final_size=(299, 299), pad_color=(255, 255, 255)):
+    x, y = img.size
+
+    # if (x < final_size[0]) and (y < final_size[1]):
+    #     new_img = Image.new("RGB", final_size, color=pad_color)
+    #     new_img.paste(img, ((final_size[0] - x) // 2, (final_size[1] - y) // 2))
+    if (x == final_size[0]) and (y == final_size[1]):
+        new_img = img
+    else:
+        new_img = Image.new("RGB", final_size, color=pad_color)
+        larger_dim = 0 if x > y else 1
+        if larger_dim == 0:
+            new_x = final_size[0]
+            new_y = int(y * new_x / x)
+        else:
+            new_y = final_size[1]
+            new_x = int(x * new_y / y)
+        img_resize = img.resize((new_x, new_y), Image.ANTIALIAS)
+        new_img.paste(img_resize, ((final_size[0] - new_x) // 2, (final_size[1] - new_y) // 2))
+
+    return new_img
 
 
 def download_image(pair):
@@ -20,8 +48,8 @@ def download_image(pair):
     :param pair: [url to the image, name of the image]
     :return:
     """
-    data_dir = "../data/img/train/"
-    resize_dim = (224, 224)
+    data_dir = "../data/img_big/train/"
+    # resize_dim = (224, 224)
 
     url = pair[0]
     ID = pair[1]
@@ -35,8 +63,10 @@ def download_image(pair):
         response = http_pool.request("GET", url)
         image = Image.open(io.BytesIO(response.data))
         image = image.convert("RGB")
-        image = image.resize(resize_dim, Image.ANTIALIAS)
-        image.save(save_path, optimize=True, quality=85)
+        if np.min(np.array(image)) > 180:  # Don't save images containing nothing
+            return
+        image = pad_image(image)
+        image.save(save_path, optimize=True)
     except Exception as e:
         print("Image '{}' could not be loaded from '{}'.\n{}".format(ID, url, e))
 
@@ -65,7 +95,7 @@ def download_data(df_path):
 
 
 if __name__ == "__main__":
-    data_dir = "../data/img/train/"
+    data_dir = "../data/img_big/train/"
 
     if not os.path.exists(data_dir):
         os.makedirs(data_dir)
